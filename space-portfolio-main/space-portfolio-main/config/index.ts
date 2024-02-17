4965447d6279a2b2493b07cd03cc544effd70d53
Shubham Bhasker <82141915+0xDracarys@users.noreply.github.com>
2024-02-17 18:00:38 +0200

    my first commit

---
+++ space-portfolio-main/space-portfolio-main/config/index.ts
@@ -0,0 +1,31 @@
+import type { Metadata } from "next";
+
+export const siteConfig: Metadata = {
+  title: "Shubham Bhasker | Cyber Wizard Portfolio",
+  description: "Welcome to my enchanted cyber wizardry portfolio.",
+  keywords: [
+    "cybersec",
+    "nextjs",
+    "wizard-portfolio",
+    "portfolio",
+    "react-icons",
+    "framer-motion",
+    "magic",
+    "encryption",
+    "ui/ux",
+    "js",
+    "javascript",
+    "typescript",
+    "web-security",
+    "html",
+    "Code",
+    "Shubham",
+    "Bhasker",
+    "Shubham Bhasker",
+    "css",
+  ] as Array<string>,
+  authors: {
+    name: "Shubham Bhasker",
+    url: "https://github.com/0xDracarys",
+  },
+} as const;
